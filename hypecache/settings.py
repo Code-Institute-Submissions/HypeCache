75db234fbf3c5a8f4d6712ebc1a83a86e2af16c4
matthew casey <matthew.casey@hotmail.co.uk>
2020-05-30 23:05:31 +0100

    bug with migration failing on heroku

--- hypecache/settings.py
+++ hypecache/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import django_heroku
+
 import os
-# import env diabled in heroku push
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
