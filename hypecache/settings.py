b985976450c766d30d16bd833882d5b23c8a3a38
matthew casey <matthew.casey@hotmail.co.uk>
2020-05-31 19:51:21 +0100

    Added: -optimised for mobile -working aws s3 image file storage and upload

--- hypecache/settings.py
+++ hypecache/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import env
+
 import django_heroku
 
 
@@ -26,8 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
+if DEBUG:
+    import env
 ALLOWED_HOSTS = ['127.0.0.1','hypecache.herokuapp.com']
 
 
